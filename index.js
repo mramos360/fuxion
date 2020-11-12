5cd5c4f63dad21c5873de89d2bdf08fdddb590ee
Mitchell Ramos <mramos@planittesting.com>
2020-11-13 08:37:03 +1000

    Updated index

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import compile from './src/interpreter/compiler.js';
 import fs from 'fs';
 
 
-export function checkUX(driver, specFilePath) {
+export default function checkUX(driver, specFilePath) {
 
     return new Promise((resolve, reject) => {
         fs.readFile(String(specFilePath), 'utf8', (err, data) => {        
             const lines = String(data).split('\n');
             const json = transform(parse(lines));
-            
+
             const result = compile(driver, transform(parse(lines))) 
 
             if (result != undefined) {
